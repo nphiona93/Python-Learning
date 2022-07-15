f8effc6e6f0b5ace297ccb9a51bde33b645bb64c
Phiona Worship <phionaworship@Phionas-MacBook-Pro.local>
2022-07-15 15:17:11 +0300

    Name and Age Inputs

---
+++ firstcode.py
@@ -0,0 +1,16 @@
+# dd = "stellar"
+# print("dd")
+# print(dd)
+# x = 3
+# y = x + 7
+# print (y)
+
+###### Inputs and  outputs ########
+
+print("Name program")
+name = input("What is your name:  ")
+print("Hello " + name) 
+
+print("Age Program")
+age = input("What is your age?  ")
+print(name + " is " +  age  + " years old")
